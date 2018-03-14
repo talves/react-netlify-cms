6077268cbc0ee5f90fa927ffa4ac8fec9802beba
talves <tony@3alves.com>
2018-03-14 15:12:52 -0700

    easy test CMS

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { init, registry } from 'netlify-cms/dist/init';
+import 'netlify-cms/dist/cms.css';
+import config from './config.json'
 
 class App extends Component {
+  componentDidMount () {
+    init({config});
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div />
     );
   }
 }
