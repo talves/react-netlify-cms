52f978c458d0c5e517df3a084a756a09a5ddbe39
talves <tony@3alves.com>
2018-03-19 11:07:22 -0700

    fixes for new global flag

--- src/components/NetlifyCMS/index.js
+++ src/components/NetlifyCMS/index.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-
-import CMS from 'netlify-cms/dist/cms';
+import './setup'
+import CMS, { init } from 'netlify-cms/dist/cms';
 import 'netlify-cms/dist/cms.css';
 import config from './data/config.json';
 import DocumentPreview from './components/DocumentPreview';
 import EditorYoutube from './components/EditorYoutube';
+
+CMS.init = init;
 
 class NetlifyCMS extends Component {
   componentDidMount () {
